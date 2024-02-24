03041939c2038d6281cee9e2645ee7cd6c17d8d6
asimnecip <necipasimarslan@gmail.com>
2024-02-24 16:29:39 +0300

    assignment done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # anchor_solution
 Advanced Solana Bootcamp - Anchor Assignment
+
+
+Assignment done! Here is the Program ID on devnet of mine:
+
+5D7Quju6iQRJ5uAHjgrrCGnQnmxbWKHHhEQFQopBhpVo
--- tests/anchor_solution.ts
+++ tests/anchor_solution.ts
@@ -4,7 +4,23 @@ import { AnchorSolution } from "../target/types/anchor_solution";
 
 describe("anchor_solution", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  // Define the network to connect to (Devnet in this case)
+  const network = "https://api.devnet.solana.com";
+  
+  // Create a connection to the Devnet cluster
+  const connection = new anchor.web3.Connection(network);
+  
+  // Get a wallet to pay for transactions and to interact with the program
+  // This assumes you have a keypair.json file in your project directory
+  // or another method of obtaining a wallet keypair
+  const wallet = anchor.Wallet.local();
+
+  // Create an Anchor provider for the Devnet cluster
+  const provider = new anchor.AnchorProvider(connection, wallet, {
+    preflightCommitment: "recent",
+  });
+  // anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.AnchorSolution as Program<AnchorSolution>;
 
